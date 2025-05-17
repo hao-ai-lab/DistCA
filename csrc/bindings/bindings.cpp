b65328a4e87df5cc6847986545ed38bcf29afa30
ZYHowell <yhzhuang@cmu.edu>
2025-05-17 21:40:32 +0000

    init draft of attention worker

---
+++ csrc/bindings/bindings.cpp
@@ -0,0 +1,18 @@
+/*
+Code modified from https://github.com/ppl-ai/pplx-kernels and subject to the MIT License.
+*/
+
+#include <torch/library.h>
+
+// #include "bindings/all_to_all_ops.h"
+#include "bindings/nvshmem.h"
+#include "bindings/registration.h"
+
+using namespace attn;
+
+TORCH_LIBRARY(attn_kernels, m) {
+  register_nvshmem_ops(m);
+//   register_all_to_all_ops(m);
+}
+
+REGISTER_EXTENSION(TORCH_EXTENSION_NAME)
---
+++ src/attn_kernels/ops.py
@@ -0,0 +1,53 @@
+# pyright: reportCallIssue=false
+
+from collections.abc import Sequence
+import logging
+import os
+
+import torch
+
+_lib_path = os.path.join(os.path.dirname(__file__), "libas_comm.so")
+torch.ops.load_library(_lib_path)
+_ops = torch.ops.attn_kernels
+
+###### NVSHMEM ######
+
+def nvshmem_get_unique_id() -> torch.Tensor:
+    return _ops.nvshmem_get_unique_id()
+
+def nvshmem_unique_id_size() -> int:
+    return _ops.nvshmem_unique_id_size()
+
+def nvshmem_alloc_empty_unique_id() -> torch.Tensor:
+    return torch.zeros(nvshmem_unique_id_size(), dtype=torch.uint8, device="cpu")
+
+def nvshmem_init(uid: torch.Tensor, rank: int, world_size: int) -> int:
+    status = _ops.nvshmem_init(uid, rank, world_size)
+    torch.cuda.synchronize()
+    return status
+
+def nvshmem_alltoall(dest: torch.Tensor, source: torch.Tensor) -> None:
+    return _ops.nvshmem_alltoall(dest, source)
+
+def nvshmem_finalize() -> None:
+    torch.cuda.synchronize()
+    _ops.nvshmem_finalize()
+
+def nvshmem_my_pe() -> int:
+    return _ops.nvshmem_my_pe()
+
+def nvshmem_n_pes() -> int:
+    return _ops.nvshmem_n_pes()
+
+def nvshmem_malloc(
+    shape: Sequence[int],
+    dtype: torch.dtype,
+    device: torch.device,
+) -> torch.Tensor:
+    return _ops.nvshmem_malloc(shape, dtype, device)
+
+def nvshmem_barrier_all() -> None:
+    _ops.nvshmem_barrier_all()
+
+def nvshmem_barrier_all_on_current_stream() -> None:
+    _ops.nvshmem_barrier_all_on_current_stream()
