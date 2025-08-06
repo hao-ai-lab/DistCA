fix bwd attnout (forward runnable; numerical wrong)

d2/runtime/megatron_patch/fused_comm_attn.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+import math
 from typing import Tuple
 
 from flash_attn.flash_attn_interface import (
@@ -277,9 +278,10 @@ class post_a2a_attn_out_with_lse(torch.autograd.Function):
             switch_buffer=switch_buffer,
             instance_id=dispatcher_id,
         )
+        hidden_size = math.prod(q.shape[1:])
         lse_size = torch.float32.itemsize // signal.itemsize
-        attn_out = recv_attn_out[:, :-lse_size]
-        softmax_lse = recv_attn_out[:, -lse_size:]
+        attn_out = recv_attn_out[:, :hidden_size].unsqueeze(1)
+        softmax_lse = recv_attn_out[:, hidden_size:hidden_size + lse_size]
         ctx.save_for_backward(
             q, k, v, attn_out, softmax_lse,
             # q_grad, k_grad send shape

tests/test_megatron_layer_bwd_resend.py
@@ -10,7 +10,7 @@ import torch
 from d2.runtime.inplace_metadata import mlp_layout_packed_params
 from d2.runtime.megatron_patch.packed_seq_params import PingPangSingleStepPackedSeqParams
 
-from test_util import create_qkv_dispath_with_backward
+from test_util import create_qkv_dispath_with_backward, simulate_communication
 from test_megatron_layer import MegatronLayerWorker, init_megatron_test
 
 
@@ -75,9 +75,74 @@ def test_forward(
     ping_pang_out, debug_out = worker.forward_ping_pang_one_stage(
         tensor_shard, ping_pang_params
     )
+    ref_debug = [None] * world_size
+    ans_debug = [None] * world_size
+    pingpong_seq_params = [None] * world_size
+    torch.distributed.all_gather_object(ref_debug, debug_ref)
+    torch.distributed.all_gather_object(ans_debug, debug_out)
+    torch.distributed.all_gather_object(pingpong_seq_params, ping_pang_params)
+    print("debug tensors gathered.")
+    if rank == 0:
+        device = torch.device("cuda", rank)
+        def to_device(o):
+            if isinstance(o, torch.Tensor):
+                return o.to(device)
+            elif isinstance(o, tuple):
+                return tuple(to_device(x) for x in o)
+            elif isinstance(o, list):
+                return [to_device(x) for x in o]
+            else:
+                return o
+        ref_debug = [to_device(debug_tensor) for debug_tensor in ref_debug]
+        ans_debug = [to_device(debug_tensor) for debug_tensor in ans_debug]
 
-    torch.testing.assert_close(normal_forward_out, ping_pang_out)
-    print("pass final result")
+        ans_debug_qkvs_pre_transfer = [ans_debug[0] for ans_debug in ans_debug]
+        # ans_debug_qkvs_post_transfer = [ans_debug[1] for ans_debug in ans_debug]
+        # ans_debug_core_attn_out = [ans_debug[2] for ans_debug in ans_debug]
+        ans_debug_core_attn_out_post_transfer = [ans_debug[1] for ans_debug in ans_debug]
+        ref_qkvs = [debug_tensor[0] for debug_tensor in ref_debug]
+        ref_attn_outs = [debug_tensor[1] for debug_tensor in ref_debug]
+        torch.testing.assert_close(ref_qkvs, ans_debug_qkvs_pre_transfer)
+        print("debug pre-layout-transfer qkv allclose")
+        ref_qs = [debug_tensor[0] for debug_tensor in ref_qkvs]
+        ref_ks = [debug_tensor[1] for debug_tensor in ref_qkvs]
+        ref_vs = [debug_tensor[2] for debug_tensor in ref_qkvs]
+        ref_qs_post_comm = simulate_communication(ref_qs, fwd_metadata_q)
+        ref_ks_post_comm = simulate_communication(ref_ks, fwd_metadata_kv)
+        ref_vs_post_comm = simulate_communication(ref_vs, fwd_metadata_kv)
+        ref_qkvs_post_comm = [
+            (ref_qs_post_comm[rank], ref_ks_post_comm[rank], ref_vs_post_comm[rank]) for rank in range(world_size)
+        ]
+        # torch.testing.assert_close(ans_debug_qkvs_post_transfer, ref_qkvs_post_comm)
+        # print("post transfer debug qkv allclose")
+
+        from flash_attn import flash_attn_varlen_func
+        ref_attn_outs_a_layout = []
+        for rank in range(world_size):
+            metadata = pingpong_seq_params[rank].to_device()
+            ref_attn_out = flash_attn_varlen_func(
+                ref_qs_post_comm[rank], ref_ks_post_comm[rank], ref_vs_post_comm[rank],
+                cu_seqlens_q = metadata.cu_seqlens_q,
+                cu_seqlens_k = metadata.cu_seqlens_kv,
+                max_seqlen_q = metadata.max_seqlen_q,
+                max_seqlen_k = metadata.max_seqlen_kv,
+                causal = True,
+                dropout_p = 0.0,
+            )
+            ref_attn_out = ref_attn_out.reshape(ref_attn_out.shape[0], 1, -1)
+            ref_attn_outs_a_layout.append(ref_attn_out)
+        ref_attn_outs_post_comm = simulate_communication(
+            ref_attn_outs_a_layout, bwd_metadata_q
+        )
+        torch.testing.assert_close(ref_attn_outs, ref_attn_outs_post_comm)
+        print("simulated attn out allclose with expected value")
+        # torch.testing.assert_close(ans_debug_core_attn_out, ref_attn_outs_a_layout)
+        # print("core attn out allclose")
+        torch.testing.assert_close(ans_debug_core_attn_out_post_transfer, ref_attn_outs)
+        print("post transfer debug attn out allclose")
+
+        torch.testing.assert_close(normal_forward_out, ping_pang_out)
+        print("pass final result")
 
 
 def test(args):