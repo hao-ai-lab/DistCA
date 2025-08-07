fix bwd attnout (fwd numerical correct; bwd runnable)

d2/runtime/attn_kernels/dispatch.py
@@ -214,7 +214,7 @@ def pre_fast_a2a_attn_out_grad_resend_qkv(
     # Padding for int4. TODO(yonghao): 
     _, pad_len = size_pad_by_int4(merged_q.shape[1], q.itemsize)
     if pad_len > 0:
-        merged_q = F.pad(merged_q, (0, _CUDA_INT4_BYTES - pad_len), mode='constant', value=0)
+        merged_q = F.pad(merged_q, (0, pad_len), mode='constant', value=0)
     return pre_fast_a2a_qkv(merged_q, k, v, kv_dispatch_mask, q_seq_tokens, k_seq_tokens,
                             q_send_buffer_offset, k_send_buffer_offset, v_send_buffer_offset,
                             is_fwd=is_fwd, instance_id=instance_id)
@@ -240,10 +240,7 @@ def post_fast_a2a_attn_out_grad_resend_qkv(
     recv_merged_q_hidden = sum(recv_q_splits)
     recv_merged_q_hidden, padding = size_pad_by_int4(recv_merged_q_hidden, recv_k.itemsize)
 
-    recv_merged_q = torch.empty(
-        (recv_attn_out_shape[0], recv_merged_q_hidden), dtype=recv_q.dtype,
-        device=recv_q.device
-    )
+    recv_merged_q = recv_k.new_empty((recv_attn_out_shape[0], recv_merged_q_hidden))
 
     recv_merged_q, recv_k, recv_v = post_fast_a2a_qkv(
         recv_merged_q, recv_k, recv_v, kv_dispatch_mask,
@@ -251,7 +248,7 @@ def post_fast_a2a_attn_out_grad_resend_qkv(
         q_recv_buffer_offset, k_recv_buffer_offset, v_recv_buffer_offset,
         is_fwd, switch_buffer=switch_buffer, instance_id=instance_id
     )
-    recv_attn_out_grad, recv_attn_out, recv_lse, recv_q = torch.split(
+    recv_attn_out_grad, recv_attn_out, recv_lse, recv_q, _ = torch.split(
         recv_merged_q, [*recv_q_splits, padding], dim=1
     )
     return (
@@ -268,7 +265,7 @@ def pre_fast_a2a_attn_out_with_lse(
     merged_attn_out = torch.concat([attn_out, softmax_lse], dim=1)
     _, pad_len = size_pad_by_int4(merged_attn_out.shape[1], attn_out.itemsize)
     if pad_len > 0:
-        merged_attn_out = F.pad(merged_attn_out, (0, _CUDA_INT4_BYTES - pad_len), mode='constant', value=0)
+        merged_attn_out = F.pad(merged_attn_out, (0, pad_len), mode='constant', value=0)
     return pre_fast_a2a_attn_out(
         merged_attn_out, send_seqlens, send_memcpy_metadata[0],
         instance_id=dispatcher_id,

d2/runtime/megatron_patch/fused_comm_attn.py
@@ -113,7 +113,7 @@ def _qkv_to_attn_out_bwd(
 
     dq, dk, dv = torch.empty_like(q), torch.empty_like(k), torch.empty_like(v)
     _wrapped_flash_attn_varlen_backward(
-        dout_padded, q, k, v, out_padded, softmax_lse, dq, dk, dv,
+        dout_padded, q.contiguous(), k, v, out_padded.contiguous(), softmax_lse, dq, dk, dv,
         cu_seqlen_q, cu_seqlen_kv, max_seqlen_q, max_seqlen_kv,
         fa_args.dropout_p, softmax_scale, fa_args.causal,
         fa_args.window_size[0], fa_args.window_size[1], fa_args.softcap, fa_args.alibi_slopes,
@@ -185,6 +185,7 @@ class FusedCommAttn(torch.autograd.Function):
 
         # Step 3: pre-dispatch attn out
         assert attn_out.shape == recv_q.shape
+        softmax_lse_dtype = softmax_lse.dtype
         softmax_lse = softmax_lse.T.contiguous().view(attn_out.dtype)
         print(456, flush=True)
         attn_out = pre_fast_a2a_attn_out_with_lse(
@@ -200,9 +201,10 @@ class FusedCommAttn(torch.autograd.Function):
             bwd_attn_out_qkv_metadata.seq_lens[1].recv_seqlens,
             *bwd_attn_out_qkv_metadata.recv_memcpy_metadata,
         ])
-        ctx.attn_out_shape = attn_out.shape
-        ctx.softmax_lse_shape = softmax_lse.shape
-        ctx.bwd_q_shape = recv_q.shape
+        recv_seqlens_q_total = bwd_attn_out_qkv_metadata.seq_lens[0].recv_seqlens.sum().item()
+        ctx.bwd_q_shape = ctx.attn_out_shape = recv_seqlens_q_total, recv_q_shape[1]
+        ctx.softmax_lse_shape = recv_seqlens_q_total, softmax_lse.shape[1]
+        ctx.softmax_lse_dtype = softmax_lse_dtype
         ctx.bwd_k_shape = bwd_attn_out_qkv_metadata.tensor_shape[1].recv_shape
         ctx.save_for_backward(*saved_tensors)
         return signal
@@ -238,7 +240,7 @@ class FusedCommAttn(torch.autograd.Function):
             switch_buffer=switch_buffer,
             instance_id=dispatcher_id,
         )
-        recv_lse = recv_lse.view(recv_attn_out_grad.dtype).T
+        recv_lse = recv_lse.view(ctx.softmax_lse_dtype).T
         # Step 2: call FA bwd.
         dq, dk, dv = _qkv_to_attn_out_bwd(
             recv_q, recv_k, recv_v, recv_attn_out, recv_attn_out_grad,
@@ -252,7 +254,7 @@ class FusedCommAttn(torch.autograd.Function):
             is_fwd=False, instance_id=dispatcher_id
         )
         signal_grad = torch.empty((1,), device=recv_q.device, dtype=recv_q.dtype)
-        return signal_grad
+        return signal_grad, *((None,) * 8)
 
 
 class post_a2a_attn_out_with_lse(torch.autograd.Function):
@@ -262,6 +264,7 @@ class post_a2a_attn_out_with_lse(torch.autograd.Function):
     @staticmethod
     def forward(ctx, signal: Tensor,
                 q: Tensor, k: Tensor, v: Tensor,
+                num_heads_q: int,
                 metadata: FastAlltoAllMetadata,
                 bwd_attn_out_qkv_metadata: FastAlltoAllMetadata,
                 dispatcher_id: int,
@@ -279,11 +282,12 @@ class post_a2a_attn_out_with_lse(torch.autograd.Function):
             instance_id=dispatcher_id,
         )
         hidden_size = math.prod(q.shape[1:])
-        lse_size = torch.float32.itemsize // signal.itemsize
+        lse_size = num_heads_q * torch.float32.itemsize // signal.itemsize
         attn_out = recv_attn_out[:, :hidden_size].unsqueeze(1)
         softmax_lse = recv_attn_out[:, hidden_size:hidden_size + lse_size]
         ctx.save_for_backward(
             q, k, v, attn_out, softmax_lse,
+            bwd_attn_out_qkv_metadata.kv_replica_mask,
             # q_grad, k_grad send shape
             bwd_attn_out_qkv_metadata.seq_lens[0].send_seqlens,
             bwd_attn_out_qkv_metadata.seq_lens[1].send_seqlens,
@@ -297,12 +301,17 @@ class post_a2a_attn_out_with_lse(torch.autograd.Function):
     def backward(ctx, grad_attn_out: Tensor):
         q, k, v, attn_out, softmax_lse, *send_metadata = ctx.saved_tensors
 
+        attn_out = attn_out.reshape(attn_out.shape[0], -1)
+        grad_attn_out = grad_attn_out.reshape(grad_attn_out.shape[0], -1)
+        q = q.reshape(q.shape[0], -1)
+        k = k.reshape(k.shape[0], -1)
+        v = v.reshape(v.shape[0], -1)
+
         pre_fast_a2a_attn_out_grad_resend_qkv(
             grad_attn_out, attn_out, softmax_lse, q, k, v,
-            None,
             *send_metadata,
             instance_id=ctx.dispatcher_id
         )
 
         signal_grad = grad_attn_out.new_empty((1,))
-        return signal_grad, *((None,) * 6)
+        return signal_grad, *((None,) * 7)

tests/test_megatron_layer.py
@@ -56,6 +56,7 @@ class MegatronLayerWorker(MegatronBaseWorker):
         tensor_input = tensor_input.cuda()
         self.layer.train()
         mlp_output, context, debug_tensors = self.layer.forward_one_stage(tensor_input, packed_seq_params=packed_seq_params)
+        mlp_output.mean().backward()
         torch.cuda.synchronize()
         print(self.rank, "ping-pong one stage forward done")
         return (mlp_output, context), debug_tensors
@@ -198,7 +199,8 @@ def init_megatron_test(
     token_bytes_q = hidden_size * dtype.itemsize
     token_bytes_kv = hidden_size * dtype.itemsize
     buffer_size = (
-        token_bytes_q * max_tokens_query +
+        token_bytes_q * max_tokens_query * 3 +
+        num_heads * torch.float32.itemsize * 2 * max_tokens_query +
         token_bytes_kv * max_tokens_key_value * max_cp_degree * 2
     )
     parallel_config = ParallelConfig(