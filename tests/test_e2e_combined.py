a88653225d503aa71b7b5c816ed4b9e058119153
GindaChen <32371474+GindaChen@users.noreply.github.com>
2025-08-31 04:48:22 +0000

    simplify worker

--- tests/test_e2e_combined.py
+++ tests/test_e2e_combined.py
@@ -76,223 +76,23 @@ def set_random_seed(seed, set_megatron: bool=True):
         tensor_parallel.model_parallel_cuda_manual_seed(seed)
 
 
-class MegatronE2eWorker(MegatronBaseWorker):
+from test_megatron_e2e import MegatronE2eWorker as BaseMegatronE2eWorker
+class MegatronE2eWorker(BaseMegatronE2eWorker):
     def __init__(self, rank: int, world_size: int):
         super().__init__(rank, world_size)
-        self.dtype = torch.bfloat16
-        self.enable_gradient_checkpointing = False
-        self.gradient_checkpointing_kwargs = {}
 
-    def init_comm(self, *args, **kwargs):
-        super().init_comm(*args, **kwargs)
+        # Patch the optimizer step function to enable nvtx range
+        optimizer_step_func = self.optimizer.step
+        def optimizer_step(*args, **kwargs):
+            with torch.cuda.nvtx.range("optimizer_step"):
+                return optimizer_step_func(*args, **kwargs)
+        self.optimizer.step = optimizer_step
 
-    def set_config(self, dtype=torch.bfloat16, enable_gradient_checkpointing=False, gradient_checkpointing_kwargs={}):
-        self.dtype = dtype
-        self.enable_gradient_checkpointing = enable_gradient_checkpointing
-        self.gradient_checkpointing_kwargs = gradient_checkpointing_kwargs
+    def loss_func(self, logits):
+        loss = logits.sum() # no gradient, but can trigger backward
+        return loss, {'loss': loss}
+    pass
 
-    def init(self, model_path, seed=42):
-        set_random_seed(seed)
-        self.model_path = model_path
-        override_model_config = OmegaConf.create()
-        override_transformer_config = OmegaConf.create({
-            "apply_rope_fusion": True,
-            # bias-act fusion
-            "bias_activation_fusion": True,
-            # no layer norm so no need for that fusion
-            # attention is in FA so no masked_softmax fusion
-            # bias-drop_out-add fusion
-            "bias_dropout_fusion": True,
-
-        })
-        # A default optim config
-        optim_config = OmegaConf.create({
-            "clip_grad": 1.0,
-            "lr": 1e-5,
-            "lr_warmup_init": 1e-5,
-            "lr_decay_steps": 1000000,
-            "lr_decay_style": 'constant',
-            "lr_warmup_steps": 1000,
-            "lr_warmup_steps_ratio": 0.0,
-            "min_lr": 1e-6,
-            "min_lr_ratio": None,
-            "total_training_steps": -1,
-            "warmup_style": "constant",
-            "weight_decay": 0.01,
-            "weight_decay_incr_style": "constant",
-            "use_checkpoint_opt_param_scheduler": False,
-            "lr_wsd_decay_style": "linear",
-
-        })
-        self._build_model_optimizer(model_path, optim_config, override_model_config, override_transformer_config)
-
-        assert self.device is not None
-        for module in self.train_module:
-            unwrap_model(module).init_ping_pong_communication_ctx(self.device)
-
-    def _init_hf_config_and_tf_config(
-        self,
-        model_path,
-        dtype,
-        override_model_config,
-        override_transformer_config,
-        trust_remote_code=True,
-    ):
-
-        # Step 1: initialize the tokenizer
-        self.local_path = model_path
-        self.tokenizer = AutoTokenizer.from_pretrained(self.local_path, trust_remote_code=trust_remote_code)
-        self.processor = AutoProcessor.from_pretrained(self.local_path, trust_remote_code=trust_remote_code)
-
-        # Step 2: get the hf
-        hf_config = AutoConfig.from_pretrained(self.local_path, trust_remote_code=trust_remote_code)
-
-        # Step 3: override the hf config
-        override_config_kwargs = {
-            "bos_token_id": self.tokenizer.bos_token_id,
-            "eos_token_id": self.tokenizer.eos_token_id,
-            "pad_token_id": self.tokenizer.pad_token_id,
-        }
-        override_config_kwargs.update(override_model_config.get("model_config", {}))
-        self.share_embeddings_and_output_weights = getattr(hf_config, "tie_word_embeddings", False)
-        update_model_config(hf_config, override_config_kwargs=override_config_kwargs)
-        self.architectures = getattr(hf_config, "architectures", None)
-        if self.rank == 0:
-            print(f"Model config after override: {hf_config}")
-        tf_config = hf_to_mcore_config(hf_config, dtype, **override_transformer_config)
-
-        def add_optimization_config_to_tf_config(tf_config):
-            # add optimization config to tf_config, e.g. checkpointing
-            if self.enable_gradient_checkpointing:
-                gradient_checkpointing_cfg = dict(self.gradient_checkpointing_kwargs)
-                tf_config.recompute_method = gradient_checkpointing_cfg.get("activations_checkpoint_method", "full")
-                tf_config.recompute_granularity = gradient_checkpointing_cfg.get(
-                    "activations_checkpoint_granularity", "full"
-                )
-                tf_config.recompute_num_layers = gradient_checkpointing_cfg.get("activations_checkpoint_num_layers", -1)
-
-        add_optimization_config_to_tf_config(tf_config)
-
-        if self.rank == 0:
-            print(f"TF config: {tf_config}")
-        self.hf_config = hf_config
-        self.tf_config = tf_config
-
-    def forward_backward_batch(self, microbatches: list[dict], forward_only: bool=False, normal_forward_fn: bool=False):
-        # TODO: for PP, since backward has a different attention layout dispatching order,
-        # we should modify the forward_backward_func here.
-
-        microbatches = [{
-            k: arg_to_cuda(v) for k, v in microbatches[0].items()
-        }]
-        for module in self.train_module:
-            unwrap_model(module).set_debug(normal_forward_fn)
-        assert len(self.train_module) == 1, "only support one module"
-
-        forward_backward_func = get_forward_backward_func()
-        n_micro_batch = len(microbatches)
-        # thd layout
-        total_seqlen = microbatches[0]['input_ids'].shape[0]
-
-        def loss_func(logits):
-            loss = logits.sum()  # no gradient, but can trigger backward
-            return loss, {'loss': loss}
-
-        def forward_step(batch_iter, model):
-            batch = next(batch_iter)
-            input_ids = batch['input_ids']
-            position_ids = batch['position_ids']
-            attention_mask = None
-            packed_seq_params = batch['packed_seq_params']
-            # returns "hidden_states" if not model.post_process (not the last layer)
-            # returns "logits" when label is None.
-            output = gptmodel_forward(
-                model, input_ids, attention_mask, position_ids, self.tf_config.sequence_parallel, packed_seq_params
-            )
-            return output, loss_func
-
-        batch_generator = make_batch_generator(microbatches, vpp_size=len(self.train_module))
-        if mpu.get_pipeline_model_parallel_world_size() > 1:
-            losses_reduced = forward_backward_func(
-                forward_step_func=forward_step,
-                data_iterator=batch_generator,
-                model=self.train_module,
-                num_microbatches=n_micro_batch,
-                seq_length=total_seqlen,  # no use, since variable_seq_lengths=True
-                micro_batch_size=1,  # no use when input_shapes was set
-                forward_only=forward_only,
-            )
-        else:
-            losses_reduced = forward_backward_func(
-                forward_step_func=forward_step,
-                data_iterator=batch_generator,
-                model=self.train_module,
-                num_microbatches=n_micro_batch,
-                seq_length=total_seqlen,  # in use for pp = 1
-                micro_batch_size=1,  # in use for pp = 1
-                forward_only=forward_only,
-            )
-
-        with torch.cuda.nvtx.range("optimizer_step"):
-            update_successful, grad_norm, num_zeros_in_grad = self.optimizer.step()
-        return losses_reduced, grad_norm
-
-    def _build_model_optimizer(self,
-        model_path, optim_config, override_model_config, override_transformer_config
-    ):
-
-        self._init_hf_config_and_tf_config(
-            model_path,
-            self.dtype,
-            override_model_config,
-            override_transformer_config,
-            True, # trust_remote_code
-        )
-
-        def make_model(wrap_with_ddp=False):
-            def megatron_actor_model_provider(pre_process, post_process):
-
-                parallel_model = init_mcore_model(
-                    self.tf_config,
-                    self.hf_config,
-                    pre_process,
-                    post_process,
-                    share_embeddings_and_output_weights=self.share_embeddings_and_output_weights,
-                    value=False,
-                    freeze_moe_router=override_model_config.get("moe_config", {}).get("freeze_moe_router", False),
-                )
-                parallel_model.to("cuda")
-                return parallel_model
-
-            override_ddp_config = OmegaConf.to_container(
-                OmegaConf.create(), resolve=True
-            )
-            return get_model(
-                megatron_actor_model_provider,
-                wrap_with_ddp=wrap_with_ddp,
-                use_distributed_optimizer=True,
-                override_ddp_config=override_ddp_config,
-            )
-
-        train_module = make_model(wrap_with_ddp=True)
-        # load_megatron_gptmodel_weights
-
-        if self.rank == 0:
-            print_model_size(train_module[0])
-
-        optim_config_megatron = init_megatron_optim_config(optim_config)
-        optimizer = get_megatron_optimizer(
-            model_chunks=train_module, config=optim_config_megatron)
-        optimizer_scheduler = get_megatron_optimizer_param_scheduler(
-            optimizer=optimizer, config=optim_config
-        )
-
-
-        self.train_module = train_module
-        self.optimizer = optimizer
-        self.optimizer_scheduler = optimizer_scheduler
-        self.hf_config = self.hf_config
-        self.optim_config = optim_config
 
 
 def init_megatron_e2e_test(
