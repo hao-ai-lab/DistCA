5e89dd98d458c279a01b488460bc8a8ff76b756c
Bo Pang <pangbo314@gmail.com>
2025-10-08 05:21:32 +0800

    ilp function interface

--- d2/planner/planner.py
+++ d2/planner/planner.py
@@ -516,7 +516,8 @@ class Planner:
                 parallel_config,
                 tolerance_factor: float = 0.1,
                 model_config = None,
-                dtype: torch.dtype = torch.bfloat16) -> None:
+                dtype: torch.dtype = torch.bfloat16,
+                planner_type: str = "greedy") -> None:
         self.model_config = model_config
         self.world_size = world_size
         self.parallel_config = parallel_config
@@ -586,8 +587,20 @@ class Planner:
         if return_items:
             return ret, items
         return ret
-    
+
     def plan_items(self, items_: list[Item], verbose=False, plot=False) -> list[Item]:
+        if self.planner_type == "greedy":
+            return self.plan_items_greedy(items_, verbose, plot)
+        elif self.planner_type == "ilp":
+            return self.plan_items_ilp(items_, verbose, plot)
+        else:
+            raise ValueError(f"Unknown planner_type: '{self.planner_type}'")
+
+    def plan_items_ilp(self, items_: list[Item], verbose=False, plot=False) -> list[Item]:
+        items = deepcopy(items_)
+        return items
+    
+    def plan_items_greedy(self, items_: list[Item], verbose=False, plot=False) -> list[Item]:
         items = deepcopy(items_)
 
         def rlog(message):
