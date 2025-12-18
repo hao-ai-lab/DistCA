9a1c56b0ab2769a6a13fc683b0b45954370c1a92
GindaChen <32371474+GindaChen@users.noreply.github.com>
2025-12-18 05:01:44 +0000

    Make proper env.sh file

--- tests/test_megatron_e2e_pipeline_with_cp.sh
+++ tests/test_megatron_e2e_pipeline_with_cp.sh
@@ -127,39 +127,18 @@ export TORCH_DIST_INIT_RETRY_TIMEOUT=30
 # ---------------------------
 # Setup paths
 # ---------------------------
+# Source the `env.sh` file from project root
+CURDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+PROJECT_ROOT="$(cd "$CURDIR/.." && pwd)"
+export PROJECT_ROOT
 
-# -- Setup CUDA
-# export CUDA_DIR=$HOME/jd/opt/cuda
-export CUDA_DIR=/usr/local/cuda-12.8
-export CUDA_HOME=$CUDA_DIR
-export PATH="$CUDA_HOME/bin:$PATH"
-
-# -- Setup NCCL
-export NCCL_HOME=$CONDA_PREFIX/lib/python3.12/site-packages/nvidia/nccl/
-export NCCL_LIB=$NCCL_HOME/lib
-export NCCL_INCLUDE_DIR=$NCCL_HOME/include
-export NCCL_LIBRARY_DIR=$NCCL_LIB
-
-# -- Setup NVSHMEM
-export NVSHMEM_PREFIX=$HOME/jd/opt/nvshmem
-export NVSHMEM_DIR=$NVSHMEM_PREFIX
-export NVSHMEM_INCLUDE=$NVSHMEM_PREFIX/include
-
-# -- Setup OPENMPI
-export OPENMPI_DIR=$HOME/jd/opt/openmpi
-export OPENMPI_INCLUDE=$OPENMPI_DIR/include
-export OPENMPI_LIBRARY_DIR=$OPENMPI_DIR/lib
-
-# -- Setup CUDNN
-export CUDNN_LIB=$CONDA_PREFIX/lib/python3.12/site-packages/nvidia/cudnn/lib
-export CUDNN_INCLUDE_DIR=$CONDA_PREFIX/lib/python3.12/site-packages/nvidia/cudnn/include
-export CUDNN_INCLUDE=$CUDNN_INCLUDE_DIR
-export CUDNN_LIBRARY_PATH=$CUDNN_LIB
-
-export LD_LIBRARY_PATH="${NVSHMEM_DIR}/lib:${CUDA_DIR}/lib64:${OPENMPI_DIR}/lib:${NCCL_LIBRARY_DIR}/:$LD_LIBRARY_PATH"
-export CPATH="${CUDNN_INCLUDE}:$CPATH"
-export PATH="${NVSHMEM_DIR}/bin:${OPENMPI_DIR}/bin:$PATH"
-
+if [ -f "$PROJECT_ROOT/env.sh" ]; then
+  # shellcheck disable=SC1091
+  source "$PROJECT_ROOT/env.sh"
+else
+  echo -e "\033[31mError: missing $PROJECT_ROOT/env.sh (computed from script location).\nPlease copy $PROJECT_ROOT/env.template.sh to $PROJECT_ROOT/env.sh and customize it.\033[0m" >&2
+  exit 1
+fi
 
 # ---------------------------
 # Setup experiment variables
